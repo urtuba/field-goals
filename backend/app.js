2b58140b21eeb8ab608c2da64c7504f7de5b27eb
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2021-12-22 01:18:52 +0300

    Add endpoints for game

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,55 @@
-const express = require('express')
-const app = express()
-const HTTP_PORT = process.env.PORT || 8080
+import Express from "express"
+import appManager from "./app-manager.js"
 
-app.listen(HTTP_PORT, () => {
-    console.log(`Server running on port ${HTTP_PORT}`)
+const app = Express()
+const port = 3000
+
+app.use(Express.json())
+
+app.get('/health', (req, res) => {
+    res.json({'status': true})
 })
 
-app.get('/', (req, res) => {
-    res.json({'message': 'OK'})
+app.post('/game', (req, res) => {
+    const gameId = appManager.addGame(req.body.team1Name, req.body.team2Name)
+    console.log(req)
+    res.json({
+        'status': true,
+        'data': {
+            'game': gameId
+        }
+    })
 })
+
+app.get('/game/all', (req, res) => {
+    res.json({
+        'status': true,
+        'data': [appManager.games] 
+    })
+})
+
+app.get('/game', (req, res) => {
+    const gameId = req.query.gameId
+    const gameManager = appManager.games[gameId]
+    const teams = gameManager.getTeams()
+    const shoots = gameManager.getAttempts()
+
+    res.json({
+        'status': true,
+        'data': {
+            'game': gameId,
+            'teams': teams,
+            'shoots': shoots
+        }
+    })
+})
+
 
 app.use((req, res, next) => {
-    res.status(404).json({'message': 'Not found'})
+    res.status(404).json({'status': false})
 })
+
+app.listen(port, () => console.log(`Field Goals app listening on port ${port}!`))
+
+
 
